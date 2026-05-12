d00a83fb0ce820f6b2cce9435dc5b16e7af333e5
Guimascena <gmascena.odm@gmail.com>
2026-05-12 18:27:14 -0300

    Add files via upload

---
+++ Exerc1Lista2/Exerc1Lista2/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exerc1Lista2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int v1 = 0;
+            int v2 = 0;
+
+            Console.WriteLine("Digite o primeiro valor:");
+            v1 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite o segundo valor:");
+            v1 = int.Parse(Console.ReadLine());
+
+            if (v1 > v2)
+            {
+                Console.WriteLine($"O maior valor é: {v1}");
+            }
+            else
+            {
+                Console.WriteLine($"O maior valor é: {v2}");
+            }
+
+           
+
+
+        }
+    }
+}
