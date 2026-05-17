fa8a5da5299fed55078aa9dec6fe6aef08ba9656
Guimascena <gmascena.odm@gmail.com>
2026-05-17 00:38:51 -0300

    Add files via upload

---
+++ Exerc11Lista2/Exerc11Lista2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exerc11Lista2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double p1 = 0, p2;
+
+            Console.WriteLine("Digite a nota P1:");
+            p1 = double.Parse(Console.ReadLine());
+
+            p2 = (15 - p1) / 2;
+
+            Console.WriteLine($"O aluno precisa tirar {p2} na P2 para ser aprovado");
+        }
+    }
+}
